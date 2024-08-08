d4d0b47b63186aad7848fe6e26b4f0cfb0edccc8
96francesco <francesco.pasanisi96@gmail.com>
2024-08-08 21:12:53 +0000

    added type hints

---
+++ src/asm_mapping/models/__init__.py
@@ -0,0 +1,3 @@
+from .lit_model_standalone import LitModelStandalone
+
+__all__ = ['LitModelStandalone']
--- src/asm_mapping/models/lit_model_standalone.py
+++ src/asm_mapping/models/lit_model_standalone.py
@@ -1,48 +1,29 @@
+# mypy: allow-untyped-defs
 import torchmetrics
 import segmentation_models_pytorch as smp # type: ignore
 import torch
+import torch.nn as nn
 import pytorch_lightning as pl
 
-from torch.optim.lr_scheduler import ReduceLROnPlateau # type: ignore
+from typing import Optional, Tuple
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torchmetrics import MetricCollection
 
 class LitModelStandalone(pl.LightningModule):
       """
       # TODO: fix docstring
-      A LightningModule for binary classification with Focal loss and Adam optimizer.
-      Implements forward pass, training, validation, test steps, and optimizer configuration.
-      
-      Attributes:
-            model (torch.nn.Module): The underlying model for predictions. 
-                  Defaults to SMP's Unet with ResNet34 backbone if None.
-            pos_weight (torch.Tensor, optional): A weight of positive examples for unbalanced datasets.
-            weight_decay (float): Weight decay (L2 penalty) for optimizer. Default: 1e-5.
-            lr (float): Learning rate for optimizer. Default: 1e-3.
-            threshold (float): Threshold for binary classification. Default: 0.5.
-            optimizer (torch.optim.Optimizer): Optimizer class. Default: torch.optim.Adam.
-            criterion (torch.nn.Module): Loss function. Default: smp.losses.FocalLoss.
-            alpha (float): Focal loss alpha parameter. Default: 0.25.
-            gamma (float): Focal loss gamma parameter. Default: 2.0.
-      
-      Methods:
-            forward(x): Defines the forward pass of the model.
-            training_step(train_batch, batch_idx): Processes a single batch during training.
-            validation_step(val_batch, batch_idx): Processes a single batch during validation.
-            test_step(test_batch, batch_idx): Processes a single batch during testing, 
-                  calculates and logs metrics.
-            configure_optimizers(): Configures and returns the model's optimizers and 
-                  learning rate schedulers.
       """
-      def __init__(self, model=None, weight_decay=1e-5,
-                 lr=1e-3, threshold=0.5, in_channels=7, alpha=0.25, gamma=2.0):
+      def __init__(self, model: Optional[nn.Module] = None, weight_decay: float =1e-5,
+                 lr: float = 1e-3, threshold: float = 0.5, in_channels: int = 6,
+                 alpha: float  =0.25, gamma: float = 2.0):
             super().__init__()
-            self.weight_decay = weight_decay
-            self.lr = lr
-            self.threshold = threshold
-            self.alpha = alpha
-            self.gamma = gamma
+            self.weight_decay: float = weight_decay
+            self.lr: float = lr
+            self.threshold: float = threshold
+            self.alpha: float = alpha
+            self.gamma: float = gamma
             self.optimizer = torch.optim.Adam
-            self.criterion = smp.losses.FocalLoss(alpha=alpha, gamma=gamma, mode='binary')
+            self.criterion: nn.Module = smp.losses.FocalLoss(alpha=alpha, gamma=gamma, mode='binary')
             self.save_hyperparameters()
 
             if model is None:
@@ -75,24 +56,26 @@ class LitModelStandalone(pl.LightningModule):
             self.val_metrics = metrics.clone(prefix='val_')
             self.test_metrics = metrics.clone(prefix='test_')
 
-      def forward(self, x):
-            return self.model.forward(x)
+      def forward(self, x: torch.Tensor) -> torch.Tensor:
+            return torch.Tensor(self.model.forward(x))
 
-      def training_step(self, train_batch, batch_idx):
+      def training_step(self, train_batch: Tuple[torch.Tensor, torch.Tensor], 
+                        batch_idx: int) -> torch.Tensor:
             x, y = train_batch
-            outputs = self(x)
+            outputs: torch.Tensor = self(x)
             y = y.unsqueeze(1).type_as(x) # add a channel dimension
-            loss = self.criterion(outputs, y)
+            loss: torch.Tensor = self.criterion(outputs, y)
             self.log('train_loss', loss, prog_bar=True, on_step=False,
                   on_epoch=True, sync_dist=True)
 
             return loss
 
-      def validation_step(self, val_batch, batch_idx):
+      def validation_step(self, val_batch: Tuple[torch.Tensor, torch.Tensor], 
+                          batch_idx: int) -> torch.Tensor:
             x, y = val_batch
             y = y.unsqueeze(1).type_as(x) # add a channel dimension
-            outputs = self(x)
-            loss = self.criterion(outputs, y)
+            outputs: torch.Tensor = self(x)
+            loss: torch.Tensor = self.criterion(outputs, y)
             
             probs = torch.sigmoid(outputs)
             preds = (probs > self.threshold).float()
@@ -103,11 +86,12 @@ class LitModelStandalone(pl.LightningModule):
             
             return loss
 
-      def test_step(self, test_batch, batch_idx):
+      def test_step(self, test_batch: Tuple[torch.Tensor, torch.Tensor], 
+                    batch_idx: int) -> torch.Tensor:
             x, y = test_batch
             y = y.unsqueeze(1).type_as(x) # convert int to float
             logits = self.model(x) # get raw logits
-            loss = self.criterion(logits, y) # compute loss
+            loss: torch.Tensor = self.criterion(logits, y) # compute loss
 
             probs = torch.sigmoid(logits) # convert logits to probabilities
             preds = (probs > self.threshold).float() # apply threshold to probrabilities
@@ -127,6 +111,12 @@ class LitModelStandalone(pl.LightningModule):
       def configure_optimizers(self):
             optimizer = self.optimizer(self.model.parameters(), lr=self.lr,
                                           weight_decay=self.weight_decay)
-            scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.8, patience=5,
-                                          verbose=True)
-            return [optimizer], [scheduler]
+            scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.8, patience=5)
+            return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                  "scheduler": scheduler,
+                  "monitor": "val_loss",
+                  "frequency": 1
+            }
+    }
