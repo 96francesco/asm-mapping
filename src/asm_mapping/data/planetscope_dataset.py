Fixed normalization

src/asm_mapping/data/planetscope_dataset.py
@@ -1,87 +1,101 @@
 # mypy: allow-untyped-defs
 import os
 import torch
-import rasterio # type: ignore
+import rasterio  # type: ignore
 import numpy as np
 import random
+import yaml
 
 from typing import List, Optional, Tuple, Dict, Any, Union
-from torchvision import transforms as T # type: ignore
-from scipy.ndimage import median_filter # type: ignore
+from torchvision import transforms as T  # type: ignore
+from scipy.ndimage import median_filter  # type: ignore
 from torch.utils.data import Dataset
 from PIL import Image
 
 from asm_mapping.data.dataset_mode import DatasetMode
 
-class PlanetScopeDataset(Dataset): # type: ignore
+
+class PlanetScopeDataset(Dataset):  # type: ignore
       """
       @TODO fix the docstring
       """
-      def __init__(self, data_dir: str, mode: DatasetMode = DatasetMode.STANDALONE,
-                   pad: bool = False, transforms: bool = False):
+
+      def __init__(self, data_dir: str, 
+                   mode: DatasetMode = DatasetMode.STANDALONE,
+                   pad: bool = False, transforms: bool = False,
+                   split: Optional[str] = None):
             self.data_dir: str = data_dir
             self.mode: DatasetMode = mode
             self.pad: bool = pad
             self.transforms: Optional[T.Compose] = None
             
+            if split:
+                  norm_path = os.path.join(os.path.dirname(__file__), "ps_normalization.yaml")
+                  with open(norm_path, 'r') as f:
+                        self.norm_values = yaml.safe_load(f)[split]
+            else:
+                  raise ValueError("Split must be provided for normalization")
+
             self.config: Dict[DatasetMode, Dict[str, Any]] = self._get_mode_config()
             self.img_folder, self.gt_folder = self._setup_folders()
             self.dataset: List[Union[str, Tuple[str, str]]] = self._create_dataset()
 
             if transforms:
-                  self.transforms  = T.Compose([
-                  T.RandomHorizontalFlip(),
-                  T.RandomVerticalFlip(),
-                  T.RandomRotation(degrees=90),
-                  T.RandomAffine(degrees=0, scale=(0.9, 1.1), shear=None)
-            ])
-            
-            self.percentiles: Tuple[float, float] = self.compute_global_percentiles()
-      
+                  self.transforms = T.Compose([
+                        T.RandomHorizontalFlip(),
+                        T.RandomVerticalFlip(),
+                        T.RandomRotation(degrees=90),
+                        T.RandomAffine(degrees=0, scale=(0.9, 1.1), shear=None),
+                        ])
+
       def _get_mode_config(self) -> Dict[DatasetMode, Dict[str, Any]]:
             return {
                   DatasetMode.STANDALONE: {
-                        'img_subfolder': 'images',
-                        'gt_subfolder': 'gt',
-                        'use_gt': True
+                        "img_subfolder": "images",
+                        "gt_subfolder": "masks",
+                        "use_gt": True,
                   },
                   DatasetMode.FUSION: {
-                        'img_subfolder': 'images/planet',
-                        'gt_subfolder': 'gt',
-                        'use_gt': True
+                        "img_subfolder": "images/planet",
+                        "gt_subfolder": "masks",
+                        "use_gt": True,
                   },
                   DatasetMode.INFERENCE: {
-                        'img_subfolder': '',
-                        'gt_subfolder': None,
-                        'use_gt': False
-                  }
+                        "img_subfolder": "",
+                        "gt_subfolder": None,
+                        "use_gt": False,
+                  },
             }
-            
+
       def _setup_folders(self) -> Tuple[str, Optional[str]]:
             config = self.config[self.mode]
-            img_folder = os.path.join(self.data_dir, config['img_subfolder'])
-            gt_folder = os.path.join(self.data_dir, config['gt_subfolder']) if config['gt_subfolder'] else None
+            img_folder = os.path.join(self.data_dir, config["img_subfolder"])
+            gt_folder = (
+                  os.path.join(self.data_dir, config["gt_subfolder"])
+                  if config["gt_subfolder"]
+                  else None
+            )
             return img_folder, gt_folder if gt_folder is not None else None
-      
+
       def _create_dataset(self) -> List[Union[str, Tuple[str, str]]]:
             dataset: List[Union[str, Tuple[str, str]]] = []
             img_filenames = sorted(os.listdir(self.img_folder))
-            
-            if self.config[self.mode]['use_gt']:
+
+            if self.config[self.mode]["use_gt"]:
                   if self.gt_folder is not None:
                         gt_filenames = sorted(os.listdir(self.gt_folder))
                         for img_name in img_filenames:
-                              index = img_name.split('_')[-1]
-                              gt_name = f'gt_{index}'
+                              index = img_name.split("_")[-1]
+                              gt_name = f"mask_{index}"
                               if gt_name in gt_filenames:
                                     img_path = os.path.join(self.img_folder, img_name)
                                     gt_path = os.path.join(self.gt_folder, gt_name)
                                     dataset.append((img_path, gt_path))
-                  else:                   
+                  else:
                         raise ValueError("Ground truth folder is None but use_gt is True")
             else:
                   dataset = [os.path.join(self.img_folder, img_name) for img_name in img_filenames]
-            
+
             return dataset
 
       @staticmethod
@@ -100,44 +114,22 @@ class PlanetScopeDataset(Dataset): # type: ignore
             ndwi = (green - nir) / (green + nir + 1e-10)
 
             return torch.from_numpy(ndwi).unsqueeze(0)
-      
 
       def __len__(self) -> int:
             return len(self.dataset)
-
-      def compute_global_percentiles(self) -> Tuple[float, float]:
-            all_values = []
-            for i in random.sample(self.dataset, min(100, len(self.dataset))):
-                  # check if i is a tuple (with GT) or a string (without GT)
-                  if isinstance(i, tuple):
-                        img_path, _ = i
-                  else:
-                        img_path = None
-
-                  with rasterio.open(img_path, 'r') as ds:
-                        img = ds.read().astype(np.float32)
-                  img = self.handle_nan_values(img)
-                  all_values.append(img)
-            
-            all_values = np.concatenate([arr.flatten() for arr in all_values])
-            
-            p2 = np.percentile(all_values, 2)
-            p98 = np.percentile(all_values, 98)
-            
-            return p2, p98
       
-      def normalize_global_percentile(self, image: np.ndarray[Any, Any]) -> np.ndarray[Any, Any]:
-            p2, p98 = self.percentiles
-            
-            normalized = (image - p2) * (1 / (p98 - p2))
-            
-            normalized = np.clip(normalized, 0, 1)
-
+      def normalize(self, image: np.ndarray[Any, Any]) -> np.ndarray[Any, Any]:
+            normalized = np.zeros_like(image)
+            bands = ['blue', 'green', 'red', 'nir']
+            for i, band in enumerate(bands):
+                  p2 = self.norm_values[band]['p2']
+                  p98 = self.norm_values[band]['p98']
+                  normalized[i] = np.clip((image[i] - p2) * (1 / (p98 - p2)), 0, 1)
             return normalized
-      
+
       def handle_nan_values(self, img: np.ndarray[Any, Any]) -> Any:
             img = np.nan_to_num(img, nan=np.nanmedian(img))
-            return median_filter(img, size=3) 
+            return median_filter(img, size=3)
 
       @staticmethod
       def dynamic_pad(image: torch.Tensor, multiple: int = 32) -> torch.Tensor:
@@ -157,22 +149,20 @@ class PlanetScopeDataset(Dataset): # type: ignore
                   h, w = image.shape
             pad_h = (multiple - h % multiple) % multiple
             pad_w = (multiple - w % multiple) % multiple
-            
-            return torch.nn.functional.pad(image, (0, pad_w, 0, pad_h), 
-                                           mode='constant', 
-                                           value=0)
 
-      def __getitem__(self, idx: int) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:                  
+            return torch.nn.functional.pad(image, (0, pad_w, 0, pad_h), mode="constant", value=0)
+
+      def __getitem__(self, idx: int) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
             item = self.dataset[idx]
             if isinstance(item, tuple):
                   img_path, gt_path = item
             else:
                   img_path = item
                   gt_path = None
-                  
+
             # read and process image
             # conversiong to float32 is done to ensure correct normalization
-            with rasterio.open(img_path, 'r') as ds:
+            with rasterio.open(img_path, "r") as ds:
                   img = ds.read().astype(np.float32)
 
             # handle NaN values
@@ -181,9 +171,9 @@ class PlanetScopeDataset(Dataset): # type: ignore
             # compute vegetation indices
             ndvi = self.ndvi(img)
             ndwi = self.ndwi(img)
-            
+
             # normalize image
-            img = self.normalize_global_percentile(img)
+            img = self.normalize(img)
 
             # convert image to tensor
             img_tensor = torch.from_numpy(img).float()
@@ -200,24 +190,24 @@ class PlanetScopeDataset(Dataset): # type: ignore
             # apply dynamic padding
             if self.pad:
                   img_tensor = self.dynamic_pad(img_tensor)
-            
+
             # if not self.is_inference:
-            if self.config[self.mode]['use_gt']:
+            if self.config[self.mode]["use_gt"]:
                   gt_path = self.dataset[idx][1]
-                  gt = np.array(Image.open(gt_path).convert('L'), dtype=np.float32)
+                  gt = np.array(Image.open(gt_path).convert("L"), dtype=np.float32)
                   gt_tensor = torch.from_numpy(gt).long()
-                  
+
                   if self.transforms:
                         gt_tensor = gt_tensor.unsqueeze(0)
                         random.seed(96)
                         torch.manual_seed(69)
                         gt_tensor = self.transforms(gt_tensor)
                         gt_tensor = gt_tensor.squeeze(0)
-                  
+
                   # apply dynamic padding
                   if self.pad:
                         gt_tensor = self.dynamic_pad(gt_tensor)
-                        
+
                   return img_tensor, gt_tensor
             else:
                   return img_tensor